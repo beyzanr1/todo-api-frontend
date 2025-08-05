c02fc63f6c64279a629d926776a89a4f209f874e
Beyzanur Solmaz <bsolmaz1134@gmail.com>
2025-08-05 22:14:28 +0300

    sidebar ve topbar componentleri eklendi

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Sidebar } from './shared/sidebar/sidebar';
+import { Topbar } from './shared/topbar/topbar';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Sidebar,
+    Topbar
   ],
   imports: [
     BrowserModule,
